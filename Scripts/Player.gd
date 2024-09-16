22f75853790b453134207779141990d4cf004218
CarlosRissati <carloseduardosimoesrissate@hotmail.com>
2024-09-16 17:46:00 -0300

    mudanças na velocidade das animações

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 var RELOAD_TIME: int = 80
 @onready var _animation_player = $AnimatedSprite2D
+var SHOOT_ANIMATION_SPEED
 @onready var _marker = $Marker2D
 var can_move: bool = true
 var bullet = preload("res://Scenes/bullet.tscn")
@@ -12,13 +13,17 @@ var cooldown: int = RELOAD_TIME
 func _ready():
 	_marker.global_position.x = position.x+24
 	_marker.global_position.y = position.y+24
+	SHOOT_ANIMATION_SPEED = _animation_player.speed_scale
 
 func _process(delta):
+	#cooldown do tiro
 	if(cooldown == 0):
 		can_shoot = true
 		cooldown = RELOAD_TIME
 	if(can_shoot == false):
 		cooldown -= 1
+
+	#movimentação do jogador
 	var horizontal = Input.get_axis("move_left", "move_right")
 	var vertical = Input.get_axis("move_up", "move_down")
 	var x: Array[int] = [horizontal, vertical]
@@ -31,6 +36,7 @@ func _process(delta):
 		position.y += vertical * SPEED * delta
 	move_and_slide()
 	
+	#Animação que irá rodar quando o jogador começar a se mover
 	if (Input.is_action_pressed("move_down") or Input.is_action_pressed("move_up") or Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")) and can_move:
 		_animation_player.play("Run")
 	elif can_move:
@@ -44,19 +50,15 @@ func _process(delta):
 		if(_marker.global_position.x == position.x - 24):
 			_marker.global_position.x += 48
 	
+	#Animação do tiro e sistema de tiro
 	if Input.is_action_just_pressed("shoot") && can_shoot:
 		can_shoot = false
-		_animation_player.play("Shoot")
 		can_move = false
+		_animation_player.play("Shoot", SHOOT_ANIMATION_SPEED)
 		var bala = bullet.instantiate()
 		bala.position = _marker.global_position
 		get_parent().add_child(bala)
 	
 	if _animation_player.frame == 3 && can_move == false:
 		can_move = true
-#func _input(event):
-#	if event.is_action_pressed("move_down") || event.is_action_pressed("move_up"):
-#		print("kkkkkkk")
-#		_animation_player.play("Run")
-#	else:
-#		_animation_player.play("Idle")
+
--- Scripts/upgrade.gd
+++ Scripts/upgrade.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func _on_area_2d_body_entered(body):
 	body.RELOAD_TIME = body.RELOAD_TIME/2
+	body.SHOOT_ANIMATION_SPEED = body.SHOOT_ANIMATION_SPEED*2
 	queue_free()
